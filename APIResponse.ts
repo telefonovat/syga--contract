4d9d83b577abc62476b8961ca507bc4bccd72c89
Khel Tan <kheltan.engineering@gmail.com>
2024-12-11 20:33:09 +0100

    Feature : Uniform APIResponse Type

---
+++ APIResponse.ts
@@ -0,0 +1,8 @@
+import { VisualizationResult } from './visualization/VisualizationResult';
+
+export interface APIResponse {
+  success: boolean;
+  message: string;
+  content: VisualizationResult | any;
+  errors?: any;
+}

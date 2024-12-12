8eb3158b3138ce6b08649882b528ca970aea78c8
Khel Tan <kheltan.engineering@gmail.com>
2024-12-12 23:26:10 +0100

    Feature : Add labels and shapes to nodes and edges

--- visualization/ComponentStyle.ts
+++ visualization/ComponentStyle.ts
@@ -1,14 +1,34 @@
-import { Node } from "./Node";
+import { Node } from './Node';
 export interface ComponentStyle {
   //Does it need to be nullable?
-  nodeColors: NodeColors,
-  edgeColors: EdgeColors,
+  nodeColors: NodeColors;
+  edgeColors: EdgeColors;
+  nodeShapes: NodeShapes;
+  edgeShapes: EdgeShapes;
+  nodeLabels: NodeLabels;
+  edgeLabels: EdgeLabels;
 }
 
 export interface NodeColors {
-  [key: Node]: string,
+  [key: Node]: string;
 }
 
 export interface EdgeColors {
-  [key: Node]: { [key: Node]: string },
+  [key: Node]: { [key: Node]: string };
+}
+
+export interface NodeShapes {
+  [key: Node]: string;
+}
+
+export interface EdgeShapes {
+  [key: Node]: { [key: Node]: string };
+}
+
+export interface NodeLabels {
+  [key: Node]: string;
+}
+
+export interface EdgeLabels {
+  [key: Node]: { [key: Node]: string };
 }
--- visualization/Legacy.ts
+++ visualization/Legacy.ts
@@ -1,49 +1,74 @@
-import type { Node } from "./Node"
-import type { Edge } from "./Edge"
+import type { Node } from './Node';
+import type { Edge } from './Edge';
 interface LegacyFrame {
-  lineno: number[],
-  console_logs?: string,  // Mark optional fields as such
-  components: LegacyComponent[],
+  lineno: number[];
+  console_logs?: string; // Mark optional fields as such
+  components: LegacyComponent[];
 }
 
 interface LegacyComponent {
-  nodes: Node[],
-  edges: Edge[],
+  nodes: Node[];
+  edges: Edge[];
   style: {
-    node_colors: { [key: Node]: string },
-    edge_colors: { [key: Node]: { [key: Node]: string } },
-  },
+    node_colors: { [key: Node]: string };
+    edge_colors: { [key: Node]: { [key: Node]: string } };
+
+    node_labels: { [key: Node]: string };
+    edge_labels: { [key: Node]: { [key: Node]: string } };
+
+    node_shapes: { [key: Node]: string };
+    edge_shapes: { [key: Node]: { [key: Node]: string } };
+  };
 }
 
 interface LegacyVisualizationResult {
-  timestamp: string,
-  res: string,
-  err?: string,
-  alg_time: number,
-  parse_time: number,
-  elapsed: number,
-  frames: LegacyFrame[],
+  timestamp: string;
+  res: string;
+  err?: string;
+  alg_time: number;
+  parse_time: number;
+  elapsed: number;
+  frames: LegacyFrame[];
 }
 
 function isLegacyFrame(object: any): object is LegacyFrame {
-  return 'lineno' in object && Array.isArray(object.lineno) &&
+  return (
+    'lineno' in object &&
+    Array.isArray(object.lineno) &&
     typeof object.lineno[0] === 'number' &&
-    (!('console_logs' in object) || !object.console_logs || typeof object.console_logs === 'string') &&
-    'components' in object && Array.isArray(object.components);
-
+    (!('console_logs' in object) ||
+      !object.console_logs ||
+      typeof object.console_logs === 'string') &&
+    'components' in object &&
+    Array.isArray(object.components)
+  );
 }
 
-function isLegacyVisualizationResult(object: any): object is LegacyVisualizationResult {
-  return 'timestamp' in object && typeof object.timestamp === 'string' &&
-    'res' in object && typeof object.res === 'string' &&
-    (!('err' in object) || !object.err || typeof object.err === 'string') &&
-    'alg_time' in object && typeof object.alg_time === 'number' &&
-    'parse_time' in object && typeof object.parse_time === 'number' &&
-    'elapsed' in object && typeof object.elapsed === 'number' &&
-    'frames' in object;
+function isLegacyVisualizationResult(
+  object: any,
+): object is LegacyVisualizationResult {
+  return (
+    'timestamp' in object &&
+    typeof object.timestamp === 'string' &&
+    'res' in object &&
+    typeof object.res === 'string' &&
+    (!('err' in object) ||
+      !object.err ||
+      typeof object.err === 'string') &&
+    'alg_time' in object &&
+    typeof object.alg_time === 'number' &&
+    'parse_time' in object &&
+    typeof object.parse_time === 'number' &&
+    'elapsed' in object &&
+    typeof object.elapsed === 'number' &&
+    'frames' in object
+  );
 }
 
 export {
-  LegacyFrame, LegacyComponent, LegacyVisualizationResult,
-  isLegacyFrame, isLegacyVisualizationResult
+  LegacyFrame,
+  LegacyComponent,
+  LegacyVisualizationResult,
+  isLegacyFrame,
+  isLegacyVisualizationResult,
 };

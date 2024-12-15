cd8320a4a1908213204cc0b3929b758473e4f1c6
Khel Tan <kheltan.engineering@gmail.com>
2024-12-15 19:56:37 +0100

    Feature : algorithms in API response content

--- APIResponse.ts
+++ APIResponse.ts
@@ -1,9 +1,10 @@
+import { Algorithm } from './user/Algorithm';
 import { JSONWebToken } from './user/Authentication';
 import { VisualizationResult } from './visualization/VisualizationResult';
 
 export interface APIResponse {
   success: boolean;
   message: string;
-  content?: VisualizationResult | JSONWebToken | any;
+  content?: VisualizationResult | JSONWebToken | Algorithm[] | any;
   errors?: any;
 }

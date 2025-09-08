5b82a18a5dfad628bf5ced96bfe84a45917e2db8
telefon <kheltan.engineering@gmail.com>
2025-09-08 14:00:27 +0200

    feat: auth request type narrowig

--- request/AuthenticateRequest.ts
+++ request/AuthenticateRequest.ts
@@ -2,3 +2,9 @@ export interface AuthenticateRequest {
   username: string;
   password: string;
 }
+
+export function isAuthenticateRequest(
+  input: any,
+): input is AuthenticateRequest {
+  return 'username' in input && 'password' in input;
+}

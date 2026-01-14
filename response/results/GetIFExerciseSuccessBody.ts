33994ae3f8f8066302e62c5ed55c949d16e72711
telefon <kheltan.engineering@gmail.com>
2026-01-14 20:26:23 +0100

    feat: add md text to IF exercise

--- response/results/GetIFExerciseSuccessBody.ts
+++ response/results/GetIFExerciseSuccessBody.ts
@@ -1,11 +1,16 @@
 import { IFOptions } from '../../domain/';
 export interface GetIFExerciseSuccessBody {
   options: IFOptions;
+  markdowntext: string;
   algorithm: string;
 }
 
 export function isGetIFExerciseSuccessBody(
   input: any,
 ): input is GetIFExerciseSuccessBody {
-  return 'options' in input && 'algorithm' in input;
+  return (
+    'options' in input &&
+    'algorithm' in input &&
+    'markdowntext' in input
+  );
 }
